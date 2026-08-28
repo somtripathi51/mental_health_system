f62cb9d8693212b6c2d776df2f9e2cb74a96a9ff
somtripathi51 <somtripathi51@gmail.com>
2026-08-29 00:43:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mental-health-system-7.onrender.com";
+  const API_BASE = "https://mental-health-system-9.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
