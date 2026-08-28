9dfaab3bdfb45a67387d497ce20844f67acad7fb
somtripathi51 <somtripathi51@gmail.com>
2026-08-28 23:24:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,26 +49,33 @@ def greet():
     return {'Welcome to Sheryians AI School Guys'}
 
 
-@app.post('/predict', response_model=PredictionResponse) #6.77777
+@app.post('/predict', response_model=PredictionResponse)
 def predict(data: StudentData):
-   
-   country_group = data.country if data.country in top_countries else "Other"
 
-   input_row = pd.DataFrame([{
-        'Age'                       :data.age,
-        'Gender'                    :data.gender,
-        'Country'                   :data.country,
-        'Academic_Level'            :data.academic_level,
-        'Most_Used_Platform'        :data.most_used_platform,
-        'Purpose_Of_Use'            :data.purpose_of_use,
-        'Avg_Daily_Usage_Hours'     :data.avg_daily_usage_hours,
-        'Daily_Unlocks'             :data.daily_unlocks,
-        'Study_Hours'               :data.study_hours,
-        'Physical_Activity_Hours'   :data.physical_activity_hours,
-        'Sleep_Hours_Per_Night'     :data.sleep_hours_per_night,
-        'Stress_Level'              :data.stress_level,
-        'Grouped_country'           :country_group
-   }])
+    country_group = (
+        data.country
+        if data.country in top_countries
+        else "Other"
+    )
 
-   prediction = model.predict(input_row)[0] #6.77
-   return PredictionResponse(predicted_mental_health_score=round(float(prediction),2))
+    input_row = pd.DataFrame([{
+        'Age': data.age,
+        'Gender': data.gender,
+        'Country': country_group,
+        'Academic_Level': data.academic_level,
+        'Most_Used_Platform': data.most_used_platform,
+        'Purpose_Of_Use': data.purpose_of_use,
+        'Avg_Daily_Usage_Hours': data.avg_daily_usage_hours,
+        'Daily_Unlocks': data.daily_unlocks,
+        'Study_Hours': data.study_hours,
+        'Physical_Activity_Hours': data.physical_activity_hours,
+        'Sleep_Hours_Per_Night': data.sleep_hours_per_night,
+        'Stress_Level': data.stress_level,
+        'Grouped_country': country_group
+    }])
+
+    prediction = model.predict(input_row)[0]
+
+    return PredictionResponse(
+        predicted_mental_health_score=round(float(prediction), 2)
+    )
